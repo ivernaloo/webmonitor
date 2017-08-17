a45736bf914bb2726cd64970245132a86b05dbad
aloo <aloocs@gmail.com>
2017-08-17 18:36:01 +0800

    visual map config

---
+++ src/view/res/js/timeline.js
@@ -0,0 +1,56 @@
+var dom = document.getElementById("container");
+var myChart = echarts.init(dom);
+var app = {};
+option = null;
+function getVirtulData(year) {
+    year = year || '2017';
+    var date = +echarts.number.parseDate(year + '-01-01');
+    var end = +echarts.number.parseDate((+year + 1) + '-01-01');
+    var dayTime = 3600 * 24 * 1000;
+    var data = [];
+    for (var time = date; time < end; time += dayTime) {
+        data.push([
+            echarts.format.formatTime('yyyy-MM-dd', time),
+            Math.floor(Math.random() * 1000)
+        ]);
+    }
+    return data;
+}
+
+
+
+option = {
+    tooltip: {
+        position: 'top'
+    },
+    visualMap: {
+        min: 0,
+        max: 1000,
+        splitNumber: 5,
+        color: ['#d94e5d','#eac736','#50a3ba'],
+        textStyle: {
+            color: '#fff'
+        },
+        orient: 'horizontal',
+        left: 'center',
+        top: 'top'
+    },
+
+    calendar: [
+        {
+            range: '2017',
+            cellSize: ['auto', 20]
+        }],
+
+    series: [{
+        type: 'heatmap',
+        coordinateSystem: 'calendar',
+        calendarIndex: 0,
+        data: getVirtulData(2017)
+    }]
+
+};
+;
+if (option && typeof option === "object") {
+    myChart.setOption(option, false);
+}
