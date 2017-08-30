53ec6b179b8f8c5af8c74c0176b17f681c190320
aloo <aloocs@gmail.com>
2017-08-30 17:32:17 +0800

    seperate the combine and storage logic

--- src/monitor.js
+++ src/monitor.js
@@ -1,12 +1,9 @@
 const CDP = require('chrome-remote-interface');
 const chromeLauncher = require('chrome-launcher');
 const _exec = require('child_process').exec;
-const low = require('lowdb');
-const db = low('db/db.json');
-const uuid = require('uuid');
 const debug = require('debug');
 const log = debug("m-monitor");
-
+const combineAndStorage = require("./utils/CombineAndStorage").load;
 
 
 start();
@@ -29,7 +26,6 @@ function start() {
     */;
 };
 
-
 function monitor(chrome) {
     log("start monitor ");
     // fake logic, mcok for test
@@ -38,10 +34,7 @@ function monitor(chrome) {
             "source": "network",
             "level": "error",
             "text": "Failed to load resource: the server responded with a status of 404 (Not Found)",
-            "timestamp": [
-                1504065079506.42,
-                1504077877726.44
-            ],
+            "timestamp": 1504097877726.44,
             "url": "http://x.zhoup.com/res/lib/lightgallery/js/lightgaller.min.js",
             "networkRequestId": "85256.11"
         }
@@ -119,83 +112,7 @@ function cleanChromePID() {
     });
 }
 
-function combineAndStorage(collection, params) {
-    var v = db.get(collection).value(),
-        r = [];
 
-    // combine time
-    // 1 and 2 => [1,2]
-    // [1,2] and 3 = > [1,3]
-    function combine(collection, object, params) {
-        let log = debug("m-combine");
-        let dict = {
-            "network": 'entry.timestamp',
-            "script" : 'timestamp',
-            "message": ''
-        };
-
-        let _a = pathResolve(object , dict[collection]),
-            _p = pathResolve(params , dict[collection]);
-
-        if (_a instanceof Array) {
-            _a = [_a[0], Math.max(_a.pop(), _p)] // [1,2] and 3 = > [1,3]
-        } else {
-            _a = [_a, _p]; // 1 and 2 => [1,2]
-        }
-
-        log(_a, _p)
-        log("return out from logic");
-        pathResolveSet(object, dict[collection], _a);
-
-        return object;
-    }
-
-    // reference : https://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key
-    // parse object and string path, such as ".path.subpath"
-    // could use curry refactor
-    function pathResolve(object, path){
-        return path.split('.')
-            .reduce((o, p)=> o ? o[p] : undefined, object)
-    }
-
-    // reference : https://stackoverflow.com/questions/13719593/how-to-set-object-property-of-object-property-of-given-its-string-name-in-ja
-    function pathResolveSet(object, path, value){
-        var i;
-        path = path.split('.');
-        for (i = 0; i < path.length - 1; i++)
-            object = object[path[i]];
-
-        object[path[i]] = value;
-    }
-
-    function compare(collection, o, params) {
-        let dict = {
-            "network": ['["entry"]["url"]', '["entry"]["text"]'],
-            "script" : ['["exceptionDetails"]["url"]', '["exceptionDetails"]["lineNumber"]', '["exceptionDetails"]["columnNumber"]'],
-            "message": ['["entry"]["url"]', '["entry"]["text"]']
-        };
-
-        return dict[collection].map(function (item) {
-
-            // compare key item in o and params
-            return new Function('return ' + "this" + item).bind(o)() == new Function('return ' + "this" + item).bind(params)()
-        }).every(function (elem) {
-            return elem;
-        });
-    }
-
-    if (v.length > 0) {
-        r = db.get(collection).value().reduce(function (m, o) {
-            return compare(collection, o, params) ?
-                [...m.slice(0, -1), combine(collection, o, params)] :
-                [...m, o, params];
-        }, []);
-    } else {
-        r.push(params)
-    }
-
-    db.set(collection, r).write();
-}
 
 
 /*        
