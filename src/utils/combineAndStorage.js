d0b0f8e93528d1c92d02c4791f4b31b381993467
aloo <aloocs@gmail.com>
2017-09-13 18:30:31 +0800

    timeline format storage

--- src/utils/combineAndStorage.js
+++ src/utils/combineAndStorage.js
@@ -1,3 +1,9 @@
+/*
+* combine And storage
+* 合并，存储日志的细节
+*
+* */
+
 /**
  * Created by xiaomi on 2017/8/30.
  */
@@ -7,6 +13,7 @@ const uuid = require('uuid');
 const debug = require('debug');
 const log = debug("m-utils-combineAndStorage");
 const DAY = 24 * 60 * 1000;
+const moment = require('moment');
 // const id = require("shortid");
 
 log("start");
@@ -15,31 +22,71 @@ log("start");
 * @param collectionName {String}
 * @param params {Object} collect information from chromeheadless
 * */
-exports.load = function(collectionName, params) {
-    var v = db.get(collectionName).value(), // storage collection list
-        log = debug("m-utils-load"),
-        r = []; // array list for store filtered data
+exports.load = function (collectionName, params) {
 
-    // v is json data
+        collection,
+        log = debug("m-utils-load"),
+        r   = [] // array list for store filtered data
+        ;
+
+
+    convertTimelineData(collectionName, params);
+};
+
+/*
+* @todo merge origin timestamp
+* */
+function mergeLogs() {
+    var v   = db.get(collectionName).value(); // storage collection list
     if (v.length > 0) {
-        /*
-        * @param m {object} previous item in the v list
-        * @param o {object} current item in the v list
-        * */
         r = v.reduce(function (m, o) {
-            return compare(collectionName, o, params) ?  // compare adjacement elements
-                [...m.slice(0, -1), combine(collectionName, o, params)] :
+            return compare(collection, o, params) ?
+                [...m.slice(0, -1), combine(collection, o, params)] :
                 [...m, o, params];
         }, []);
     } else {
-        // params.id = id.generate();
         r.push(params)
     }
 
-    // log("id : ", id.generate());
-    // log("r : ", r);
-    db.set(collectionName, r).write();
-};
+    db.set(collection, r).write();
+
+}
+
+/*
+    * @todo build formated data
+    * such as :
+    * network : [[date:number] .... ]
+    * scripts : [[date:number] .... ]
+    * message : [[date:number] .... ]
+    * total : [[date:number] .... ]
+    * */
+function convertTimelineData(collectionName, params) {
+    var collection,
+        date = moment(params.timestamp).format('l');
+
+    // read the data
+    // load "count-" _ collectionName
+    collection = db.get("count-" + collectionName).value();
+
+    log("collection : ", collection);
+    // updateOrInsert
+    // v is json data
+    if (collection.length > 0) {
+        log("record exist");
+        collection.map(function (x) {
+            if (x[0] == date) {
+                ++x[1];
+            }
+            return [x[0], x[1]]
+        });
+    } else {
+        // params.id = id.generate();
+        collection.push([date, 1])
+    }
+
+    // save
+    db.set("count-" + collectionName, collection).write();
+}
 
 // combine last item in the array
 // combine time
@@ -58,20 +105,17 @@ function combine(collectionName, object, params) {
         "message": ''
     };
 
-    let _a = pathResolve(object , dict[collectionName]),
-        _p = pathResolve(params , dict[collectionName]); // probably, return single number or array
+    let _a = pathResolve(object, dict[collectionName]),
+        _p = pathResolve(params, dict[collectionName]); // probably, return single number or array
 
-    log("_a : ", _a);
-    log("_p : ", _p);
 
     if (_a instanceof Array) {
         log("max combine", _p - _a.pop() > DAY);
-         // @todo change to index storage, such as {date: data}
-         // @todo change to moment support
-         // @todo where lowdb support link
+        // @todo change to index storage, such as {date: data}
+        // @todo change to moment support
+        // @todo where lowdb support link
         // _a maybe : xx , [xx,xx], [[xxx,xx],[xx,xx]], [[xxx,xxx],xx]
-         _p.
-        /*
+        _p./*
         if( _a.pop() instanceof  Array ){
             // compare the last time
 
@@ -79,7 +123,7 @@ function combine(collectionName, object, params) {
 
         }
         */
-        _a = [_a[0], Math.max(_a.pop(), _p)] // [1,2] and 3 = > [1,3]
+            _a = [_a[0], Math.max(_a.pop(), _p)] // [1,2] and 3 = > [1,3]
     } else {
         log("direct combine")
         _a = [_a, _p]; // 1 and 2 => [1,2]
@@ -95,13 +139,13 @@ function combine(collectionName, object, params) {
 // reference : https://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key
 // parse object and string path, such as ".path.subpath"
 // could use curry refactor
-function pathResolve(object, path){
+function pathResolve(object, path) {
     return path.split('.')
         .reduce((o, p)=> o ? o[p] : undefined, object)
 }
 
 // reference : https://stackoverflow.com/questions/13719593/how-to-set-object-property-of-object-property-of-given-its-string-name-in-ja
-function pathResolveSet(object, path, value){
+function pathResolveSet(object, path, value) {
     var i;
     path = path.split('.');
     for (i = 0; i < path.length - 1; i++)
